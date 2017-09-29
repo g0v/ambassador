9c44bc9a13c513a31d3eef30a3e44aec0a6c686f
caasi Huang <caasi.igd@gmail.com>
2017-09-29 17:31:17 +0800

    Sync logbot channel date with the store

--- src/components/LogbotPage/index.js
+++ src/components/LogbotPage/index.js
@@ -4,11 +4,44 @@ import { connect } from 'react-redux'
 import * as actions from '~/actions'
 import { mapDispatchToProps } from '~/types/action'
 import * as L from '~/types/logbot'
+import moment from 'moment'
 import styles from './index.css'
 
 class LogbotPage extends PureComponent {
   static defaultProps = {
     className: '',
+  }
+
+  handleLogbotAction = (e) => {
+    if (e.origin !== 'https://logbot.g0v.tw') return
+
+    const { actions } = this.props
+
+    switch (e.data.type) {
+      case 'LOGBOT_DATE': {
+        let { date } = e.data
+        if (date === 'today') {
+          date = moment().format(L.DATE_FORMAT)
+        }
+        actions.logbot.setDate(date)
+        return
+      }
+      case 'LOGBOT_MESSAGE': {
+        // TODO: push current message index and its content to a stack
+        console.log('message index', e.data.index)
+        return
+      }
+      default:
+        return
+    }
+  }
+
+  componentDidMount() {
+    global.addEventListener('message', this.handleLogbotAction)
+  }
+
+  componentWillUnmount() {
+    global.removeEventListener('message', this.handleLogbotAction)
   }
 
   render() {
