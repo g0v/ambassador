8c2bb5741a71d0c2c1370ca805c24d471a44f210
caasi Huang <caasi.igd@gmail.com>
2017-10-07 05:10:01 +0800

    Refine types. But they are still Broken.

--- src/store/app.js
+++ src/store/app.js
@@ -5,18 +5,15 @@ import type { PlainAction, Action, RawAction } from '~/types/action'
 import type { State } from '~/reducers'
 
 // $FlowFixMe: unable to inference function types in a union type
-type InjectStore<A, B> = RawAction<A, B> => Action<A, B>
-export type Dispatch
-  = DispatchAPI<PlainAction>
-  | InjectStore<*, *>
+export type Dispatch = DispatchAPI<PlainAction> & (<A, B>(RawAction<A, B>) => Action<A, B>)
 // check: https://github.com/facebook/flow/issues/1948
 
 export type StoreAPI = MiddlewareAPI<State, PlainAction, Dispatch>
 
-// $FlowFixMe: PlainAction is not a function type and RawAction is a function type
 export type App = Middleware<State, PlainAction, Dispatch>
 const app: App = store => next => action =>
   typeof action === 'function'
     ? action(store)
     : next(action)
+
 export default app
