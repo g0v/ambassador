a8ab0e9eea462a0b0179aa97c5b406b8e8cce03c
caasi Huang <caasi.igd@gmail.com>
2017-11-09 20:17:16 +0800

    Extract keywords and normalize nicknames

--- server/search.js
+++ server/search.js
@@ -1,5 +1,13 @@
-const R = require('ramda')
+const fs = require('fs-extra')
+const path = require('path')
+const axios = require('axios')
 const moment = require('moment')
+const jieba = require('nodejieba')
+const R = require('ramda')
+
+const logbotUrl = process.env.LOGBOT_URL || 'http://example.com'
+const getUrl = (date) => `${logbotUrl}/channel/g0v.tw/${date}/json`
+const dataPath = path.resolve(__dirname, 'data')
 
 // XXX: duplicated
 const DATE_FORMAT = 'YYYY-MM-DD'
@@ -15,8 +23,58 @@ const dates = function *(begin, end) {
   } while(date !== end)
 }
 
-const takeLastThree = R.compose(R.take(3), R.reverse)
-
 const begin = process.env.DDAY
 const end = moment().format(DATE_FORMAT)
-console.log(takeLastThree([...dates(begin, end)]))
+
+const main = () => {
+  const ds = dates(begin, end)
+
+  // XXX: `jieba.cut*` is unusable without traditional chinese dictionary
+  jieba.load()
+
+  fs.ensureDir(dataPath)
+    .then(() => {
+      let ps = []
+
+      for (let d of ds) {
+        // TODO: skip existing files
+        const outputPath = path.resolve(dataPath, `${d}.json`)
+        const p = fs.exists(outputPath)
+          .then(exists =>
+            exists
+              ? true
+              : axios.get(getUrl(d))
+                  .then(d => d.data)
+                  .then(R.map(extractKeywords))
+                  .then(ls => {
+                    console.log(`extracting keywords of ${d} ...`)
+                    return fs.writeJson(outputPath, ls, { spaces: 2 })
+                  })
+          )
+        ps.push(p)
+      }
+
+      return Promise.all(ps)
+    })
+    .catch(err => console.error(err))
+}
+
+const extractKeywords = (log) => {
+  const ks = jieba.extract(log.msg, Number.MAX_SAFE_INTEGER)
+  let keywords = [normalizeNick(log.nick)]
+
+  for (k of ks) keywords.push(k.word)
+
+  return Object.assign({}, log, { keywords })
+}
+
+// TODO
+const normalizeNick = (nick) => {
+  return (
+    nick
+      .replace(/^ⓢ\s*/, '')
+      .replace(/^🅣\s*/, '')
+      .replace(/_+$/, '')
+  )
+}
+main()
