ca55d512d98a3501a646e09087f274e5fb08800d
caasi Huang <caasi.igd@gmail.com>
2017-12-22 02:41:21 +0800

    Fix process.env.HOST in production build

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
+process.env.HOST = 'kinmen-tao-884344.middle2.me'
 process.env.API_HOST = 'kinmen-tao-884344.middle2.me'
 process.env.DDAY = '2013-07-26'
 
