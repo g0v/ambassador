5c1f3454d272489a5ff22513b4f6ef946ec5aba5
caasi Huang <caasi.igd@gmail.com>
2018-02-16 05:25:21 +0800

    Use JSON schema to validate v1 g0v.json files

--- bin/group.js
+++ bin/group.js
@@ -1,15 +1,16 @@
 import fs from 'fs-extra'
 import path from 'path'
 import * as paths from './paths'
+import Ajv from 'ajv'
+import v1 from './schema/v1.json'
 
 let group = {}
 
-let indent = 0
-const log = (arg, ...args) => {
-  let padding = ''
-  for (let i = 0; i < indent; ++i) padding += '  '
-  console.log(padding + arg, ...args)
-}
+const ajv = new Ajv()
+const validate = ajv.compile(v1)
+
+let total = 0
+let passed = 0
 
 // main
 ;(async () => {
@@ -23,32 +24,19 @@ const log = (arg, ...args) => {
       const filepath = path.resolve(repopath, repo, 'g0v.json')
       const data = await fs.readJson(filepath)
 
-      console.log(`${user}/${repo}:`)
+      const valid = validate(data)
+      if (valid) {
+        ++passed
+        console.log(`${user}/${repo}: o`)
+      } else {
+        console.log(`${user}/${repo}: x`)
+        const errorpath = path.resolve(repopath, repo, 'errors.json')
+        await fs.writeJson(errorpath, validate.errors, { spaces: 2 })
+      }
 
-      indent = 1
-      if (!data.author) log('author is missing')
-      if (!data.status) log('status is missing')
-      if (!data.name) log('name is missing')
-      if (!data.name_zh) log('name_zh is missing')
-      if (!data.description) log('description is missing')
-      if (!data.description_zh) log('description_zh is missing')
-      if (!data.homepage) log('homepage is missing')
-      if (!data.thumbnail) log('thunmbnail is missing')
-      if (!data.document) log('document is missing')
-      if (!data.repository) log('repository is missing')
-      if (!data.licenses) log('licenses are missing')
-      if (data.license) log('misspell licenses as license')
-      if (!data.keywords) log('keywords are missing')
-      if (data.keyword) log('misspell keywords as keyword')
-      if (!data.audience) log('audience is missing')
-      if (!data.products) log('proudcts are missing')
-      if (data.product) log('misspell products as product')
-      if (!data.projects) log('projects are missing')
-      if (data.project) log('misspell projects as project')
-      if (!data.contributors) log('contributors are missing')
-      if (data.contributor) log('misspell contributors as contributor')
-      if (!data.needs) log('needs are missing')
-      if (data.need) lag('misspell needs as need')
+      ++total
     }
   }
+
+  console.log(`\n${passed}/${total} g0v.json are valid`)
 })()
