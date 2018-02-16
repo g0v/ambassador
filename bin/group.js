f5dfdb8425e69e0a950844a43703f9a5e0308feb
caasi Huang <caasi.igd@gmail.com>
2018-02-16 18:12:52 +0800

    Group repos

--- bin/group.js
+++ bin/group.js
@@ -3,8 +3,16 @@ import path from 'path'
 import * as paths from './paths'
 import Ajv from 'ajv'
 import v1 from './schema/v1.json'
+import { forEach } from 'ramda'
 
 let group = {}
+const groupAppend = o => name => {
+  if (!group[name]) {
+    group[name] = []
+  }
+
+  group[name].push(o)
+}
 
 const ajv = new Ajv()
 const validate = ajv.compile(v1)
@@ -24,6 +32,14 @@ let passed = 0
       const filepath = path.resolve(repopath, repo, 'g0v.json')
       const data = await fs.readJson(filepath)
 
+      // v1
+      if (data.partOf) {
+        groupAppend(data.name)(data.partOf)
+      }
+      if (data.projects) {
+        forEach(groupAppend(data.name), data.projects)
+      }
+
       const valid = validate(data)
       if (valid) {
         ++passed
@@ -35,8 +51,13 @@ let passed = 0
       }
 
       ++total
+
+      // TODO: v2
     }
   }
+
+  const filepath = path.resolve(paths.metadata, 'group.v1.json')
+  await fs.writeJson(filepath, group, { spaces: 2 })
 
   console.log(`\n${passed}/${total} g0v.json are valid`)
 })()
