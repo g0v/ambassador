5fa747add508ef255d4a41a48d1088fe1656200e
caasi Huang <caasi.igd@gmail.com>
2018-02-18 04:53:24 +0800

    Aggregate patched(v2) g0v.json files for group.v2.json

--- bin/group.js
+++ bin/group.js
@@ -5,38 +5,6 @@ import Ajv from 'ajv'
 import v1 from './schema/v1.json'
 import { forEach, uniq } from 'ramda'
 
-let group = {}
-const groupConcat = o => name => {
-  var g = group[name]
-
-  if (!g) {
-    g = group[name] = {
-      children: [],
-      keywords: [],
-      products: [],
-      contributors: [],
-      needs: []
-    }
-  }
-
-  g.children.push(o.name)
-  if (o.keywords) g.keywords = g.keywords.concat(o.keywords)
-  if (o.products) g.products = g.products.concat(o.products)
-  if (o.contributors) g.contributors = g.contributors.concat(o.contributors)
-  if (o.needs) g.needs = g.needs.concat(o.needs)
-}
-
-const groupCleanup = () => {
-  for (const k in group) {
-    const g = group[k]
-
-    g.keywords = uniq(g.keywords)
-    g.products = uniq(g.products)
-    g.contributors = uniq(g.contributors)
-    g.needs = uniq(g.needs)
-  }
-}
-
 const ajv = new Ajv()
 const validator = {
   v1: ajv.compile(v1),
@@ -45,12 +13,87 @@ const validator = {
 
 let result = {
   v1: {
+    group: {},
     total: 0,
     valid: 0
   },
   v2: {
+    group: {},
     total: 0,
     valid: 0
+  }
+}
+
+const groupConcat = (version, o) => name => {
+  // could be ""
+  if (!name) return
+
+  if (version === 'v1') {
+    let g = result.v1.group[name]
+
+    if (!g) {
+      g = result.v1.group[name] = {
+        children: [],
+        thumbnails: [],
+        keywords: [],
+        products: [],
+        contributors: [],
+        needs: []
+      }
+    }
+
+    g.children.push(o.name)
+    if (o.thumbnail) g.thumbnails.push(o.thumbnail)
+    if (o.keywords) g.keywords = g.keywords.concat(o.keywords)
+    if (o.products) g.products = g.products.concat(o.products)
+    if (o.contributors) g.contributors = g.contributors.concat(o.contributors)
+    if (o.needs) g.needs = g.needs.concat(o.needs)
+  }
+
+  if (version === 'v2') {
+    let g = result.v2.group[name]
+
+    if (!g) {
+      g = result.v2.group[name] = {
+        children: [],
+        thumbnails: [],
+        keywords: [],
+        products: [],
+        contributors: [],
+        needs: []
+      }
+    }
+
+    let url = o && o.repository && o.repository.url
+    if (url) g.children.push(url)
+    if (o.thumbnail) g.thumbnails.push(o.thumbnail)
+    if (o.keywords) g.keywords = g.keywords.concat(o.keywords)
+    if (o.products) g.products = g.products.concat(o.products)
+    if (o.contributors) g.contributors = g.contributors.concat(o.contributors)
+    if (o.needs) g.needs = g.needs.concat(o.needs)
+  }
+}
+
+const groupCleanup = () => {
+  for (const k in result.v1.group) {
+    const g = result.v1.group[k]
+
+    g.thumbnails = uniq(g.thumbnails)
+    g.keywords = uniq(g.keywords)
+    g.products = uniq(g.products)
+    g.contributors = uniq(g.contributors)
+    g.needs = uniq(g.needs)
+  }
+
+  for (const k in result.v2.group) {
+    const g = result.v2.group[k]
+
+    g.thumbnails = uniq(g.thumbnails)
+    g.keywords = uniq(g.keywords)
+    // TODO: product is not a string anymore
+    g.products = uniq(g.products)
+    g.contributors = uniq(g.contributors)
+    g.needs = uniq(g.needs)
   }
 }
 
@@ -65,15 +108,15 @@ let result = {
     const repos = await fs.readdir(repopath)
 
     for (const repo of repos) {
-      const filepath = path.resolve(repopath, repo, 'g0v.json')
-      const data = await fs.readJson(filepath)
+      let filepath = path.resolve(repopath, repo, 'g0v.json')
+      let data = await fs.readJson(filepath)
 
       // v1
       if (data.partOf) {
-        groupConcat(data)(data.partOf)
+        groupConcat('v1', data)(data.partOf)
       }
       if (data.projects) {
-        forEach(groupConcat(data), data.projects)
+        forEach(groupConcat('v1', data), data.projects)
       }
 
       const valid = validator.v1(data)
@@ -88,16 +131,36 @@ let result = {
 
       ++result.v1.total
 
-      // TODO: v2
+      // v2
+      filepath = path.resolve(paths.publicData, user, repo, 'master.patch.json')
+      try {
+        const patch = await fs.readJson(filepath)
+        data = { ...data, ...patch }
+
+        if (data.group) {
+          groupConcat('v2', data)(data.group)
+        }
+
+        // TODO: validation
+      } catch (err) {
+        console.log(`can't patch ${user}/${repo}!`)
+      }
+
       ++result.v2.total
     }
   }
 
   groupCleanup()
-  const filepath = path.resolve(paths.metadata, 'group.v1.json')
-  await fs.writeJson(filepath, group, { spaces: 2 })
+  console.log('')
 
-  console.log(`\n${result.v1.valid}/${result.v1.total} g0v.json are valid`)
+  let filepath
+  filepath = path.resolve(paths.metadata, 'group.v1.json')
+  await fs.writeJson(filepath, result.v1.group, { spaces: 2 })
+  console.log(`${result.v1.valid}/${result.v1.total} v1 g0v.json are valid`)
+
+  filepath = path.resolve(paths.metadata, 'group.v2.json')
+  await fs.writeJson(filepath, result.v2.group, { spaces: 2 })
+  console.log(`${result.v2.valid}/${result.v2.total} v2 g0v.json are valid`)
 })()
   .catch(err => {
     console.error(err)
