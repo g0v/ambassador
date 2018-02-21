c196a6b522d001042c5654fd745ee344022c2d7c
caasi Huang <caasi.igd@gmail.com>
2018-02-21 22:04:40 +0800

    Update the domain name

--- scripts/build-client.js
+++ scripts/build-client.js
@@ -3,9 +3,9 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
-process.env.HOST = 'kinmen-tao-884344.middle2.me'
-process.env.API_HOST = 'kinmen-tao-884344.middle2.me'
-process.env.DDAY = '2013-07-26'
+process.env.HOST = 'amb.g0v.tw';
+process.env.API_HOST = 'amb.g0v.tw';
+process.env.DDAY = '2013-07-26';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
